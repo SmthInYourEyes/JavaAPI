cbb24b139aca2c52f75f82664b9735ade7224b09
SmthInYourEyes <smthinyoureyes@hotmail.com>
2024-04-06 21:20:09 +0300

    Первый тест

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
