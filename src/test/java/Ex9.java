deaef64548c2ad58aadf3bb0449ea665d14e8e23
SmthInYourEyes <smthinyoureyes@hotmail.com>
2024-04-16 14:49:16 +0300

    Дз9 черновой

---
+++ src/test/java/Ex9.java
@@ -0,0 +1,46 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex9 {
+    @Test
+    public void passSelection() {
+        String login = "super_admin";
+        List<String> passwords = Arrays.asList(
+                "123456", "123456789", "qwerty", "password", "1234567",
+                "12345678", "12345", "iloveyou", "111111", "123123",
+                "abc123", "qwerty123", "1q2w3e4r", "admin", "qwertyuiop",
+                "654321", "555555", "lovely", "7777777", "welcome",
+                "888888", "princess", "dragon", "password1", "123qwe"
+        );
+
+        for(String password: passwords){
+            Response response = RestAssured
+                    .given()
+                    .formParam("login", login)
+                    .formParam("password", password)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+
+            String authCookie = response.getCookie("auth_cookie");
+            System.out.println(authCookie);
+
+            response = RestAssured
+                    .given()
+                    .cookie("auth_cookie", authCookie)
+                    .when()
+                    .get("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                    .andReturn();
+
+            response.prettyPrint();
+
+
+        }
+
+    }}
+
