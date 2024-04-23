08cf3fdcd1ef626f83dd8802ab8cd5df9188f44c
SmthInYourEyes <smthinyoureyes@hotmail.com>
2024-04-23 17:21:15 +0300

    Дз12

---
+++ src/test/java/testsWriting/Ex12.java
@@ -0,0 +1,25 @@
+package testsWriting;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Ex12 {
+    @Test
+    public void cookieTest() {
+        //Узнаем что за header
+        Response response = RestAssured
+                .get(" https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        Headers headers = response.getHeaders();
+        System.out.println(headers);
+
+        //Сравнивем полученный header с ожидаемым
+        String cookie = response.getHeader("x-secret-homework-header");
+        assertEquals("Some secret value", cookie);
+    }
+}
