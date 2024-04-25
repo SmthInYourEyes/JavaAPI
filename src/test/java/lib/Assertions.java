e484421836423c2dbcc0884419079fa5119c63a7
SmthInYourEyes <smthinyoureyes@hotmail.com>
2024-04-25 14:54:12 +0300

    урок 3

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -2,7 +2,9 @@ package lib;
 
 import io.restassured.response.Response;
 
+
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -28,7 +30,19 @@ public class Assertions {
                 "Response status code is not expected");
     }
 
-    public static void assertJsonHasKey(Response Response, String expectedFieldName) {
+    public static void assertJsonHasField(Response Response, String expectedFieldName) {
         Response.then().assertThat().body("$", hasKey(expectedFieldName));
     }
+
+    public static void assertJsonHasFields(Response Response, String[] expectedFieldNames) {
+        for (String expectedFieldName : expectedFieldNames) {
+            Assertions.assertJsonHasField(Response, expectedFieldName);
+        }
+    }
+
+    public static void assertJsonHasNotField(Response Response, String unexpectedFieldName) {
+        Response.then().assertThat().body("$", not(hasKey(unexpectedFieldName)));
+    }
+
+
 }
